33f377d50480f1cf2af9e8ed6c31cd74a923a483
huangjian <1776549643@qq.com>
2019-02-15 16:58:23 +0800

    m

--- 한국어/Tool/insertWord.py
+++ 한국어/Tool/insertWord.py
@@ -10,21 +10,25 @@ import json, random, sys, io
     https://blog.csdn.net/weixin_42737442/article/details/82027328
 """
 
-file = io.open('./word.json', 'r+')
+file = io.open('../word.json', 'r+')
 list = json.load(file)
 
 zh = raw_input("请输入中文：")
 kr = raw_input("请输入韩文：")
+print "您输入的单词是：[{}] - [{}]\n".format(zh, kr)
 
-new_item = {
-    "zh": zh.decode("utf-8"),
-    "kr": kr.decode("utf-8"),
-    "weight": 0
-}
-list.append(new_item)
+if raw_input("确认插入？").strip() == "":
+    new_item = {
+        "zh": zh.decode("utf-8"),
+        "kr": kr.decode("utf-8"),
+        "weight": -8
+    }
+    list.append(new_item)
 
-text = json.dumps(list, ensure_ascii=False, sort_keys=True, indent=2, separators=(',', ': '))
-file.seek(0)
-file.truncate()
-file.write(text)
+    text = json.dumps(list, ensure_ascii=False, sort_keys=True, indent=2, separators=(',', ': '))
+    file.seek(0)
+    file.truncate()
+    file.write(text)
+    print "插入成功！"
+
 file.close()
